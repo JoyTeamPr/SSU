5f698b0fc0137f30a2653dbb546335e26137cf9b
Dmitriy <dimamylykob@gmail.com>
2022-10-07 10:11:24 +0400

    Seminar 1

--- Seminar1/Seminar1.cpp
+++ Seminar1/Seminar1.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Do what you can't";
+    double r, p, a, b, c;
+    const double PI = acos(-1, 0);
+    cout << "Введите радиус";
+    cin >> r;
+    cout << "Длина окружности = " << 2 * PI * r << "\n" << "Площадь круга = " << PI * pow(r, 2);
+    
+    cout << "Введите три стороны через пробел";
+    cin >> a >> b >> c;
+    p = (a + b + c) / 2;
+    cout << p;
 }
