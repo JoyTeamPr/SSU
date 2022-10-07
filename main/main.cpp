fddcd5bec748b9009871edfe33db61ef3e421a2f
Dmitriy <dimamylykob@gmail.com>
2022-10-07 10:40:17 +0400

    1-I (13)

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b, ans;
+    cout << "Введите значения a и b\n";
+    cin >> a, b;
+    cout << "Результат = " << (pow(a, 2) + pow(b, 2)) / (1 - ((pow(a, 3) - b) / 3));
+}
