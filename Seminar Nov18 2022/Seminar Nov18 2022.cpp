1affd56bf4ea5d779349999b6c8168cd4c7d7db7
Dmitriy <dimamylykob@gmail.com>
2022-11-17 19:50:01 +0400

    Seminar 18.11.22

---
+++ Seminar Nov18 2022/Seminar Nov18 2022.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+double a(double s)
+{
+	return sqrt(4 * s / sqrt(3));
+}
+
+double hypotenuse(double a)
+{
+	return sqrt(a * a + a * a);
+}
+
+bool isZero(double &num)
+{
+	if (num == 0)
+	{
+		return false;
+	}
+	else return true;
+}
+
+double pluss(double f, double s)
+{
+	return (f + s);
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double s, cathet, num, first, second;
+	cout << "1. a) Введите площадь равностороннего треугольника\n";
+	cin >> s;
+	cout << "Сторона этого треугольника равна " << a(s) << endl;
+	cout << "   б) Введите катет равнобедренного треугольника\n";
+	cin >> cathet;
+	cout << "Гипотенуза этого треугольника равна " << hypotenuse(cathet) << endl;
+	cout << "   в) Непонятно. \n"; // уточнить на семинаре
+	cout << "3. Введите а\n";
+	cin >> num;
+	if (isZero(num))
+	{
+		cout << num << " = 0";
+	}
+	else cout << num << " не равно 0";
+	cout << "4. Введите два числа\n";
+	cin >> first >> second;
+	cout << pluss(first, second);
+}
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,21 @@
 ﻿#include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main()
 {
-    return 0;
+    int a, b, flag = 0;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        flag = 0;
+        for (int d = i - 1; d > 1; d--)
+        {
+            if (i % d == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) cout << i << ' ';
+    }
 }
